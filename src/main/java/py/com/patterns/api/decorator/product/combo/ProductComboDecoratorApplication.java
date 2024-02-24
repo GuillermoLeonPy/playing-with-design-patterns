b58fc809bc011937278c211df6ea094c8330a079
Guillermo Leon <guillermo.leon.py@gmail.com>
2024-02-24 13:35:58 -0300

    Lambda expression

--- src/main/java/py/com/patterns/api/decorator/product/combo/ProductComboDecoratorApplication.java
+++ src/main/java/py/com/patterns/api/decorator/product/combo/ProductComboDecoratorApplication.java
@@ -1,7 +1,5 @@
 package py.com.patterns.api.decorator.product.combo;
 
-import java.util.function.Consumer;
-
 public class ProductComboDecoratorApplication {
 
 	public static void main(String[] args) {
@@ -9,14 +7,8 @@ public class ProductComboDecoratorApplication {
 		Product product = new Product("Internet 60 M", new Product("Bonus Mobile Internet Free", new Product("Hi Bandwidth Extensor Router", new Product("TV Decoder", null))));
 
 		StringBuffer sb = new StringBuffer();
-		product.decorate().forEach(new Consumer<ProductFeature>() {
-
-			@Override
-			public void accept(ProductFeature t) {
-				// TODO Auto-generated method stub
-				sb.append("\n" + t.getName());
-			}
-		});
+		
+		product.decorate().forEach(p -> sb.append("\n" + p.getName()));
 		
 		System.out.println("Product stack: " + sb.toString());
 	}
